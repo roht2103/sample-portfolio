65c10ef5fbc372ed77a92ab73f31d09c23974431
Rohit Thorat <109894186+roht2103@users.noreply.github.com>
2022-08-02 11:54:14 +0530

    dsc

---
+++ app.js
@@ -0,0 +1,14 @@
+VANTA.GLOBE({
+  el: "#vanta",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.0,
+  minWidth: 200.0,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  color: 0xe3d4d4,
+  color2: 0xcabfbf,
+  backgroundColor: 0xfdf8f3,
+  size: 1,
+});
